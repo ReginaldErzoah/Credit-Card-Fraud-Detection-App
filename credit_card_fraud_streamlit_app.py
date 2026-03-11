22536e9cb689d81d7aef53b160755f6fb529e379
Reginald Erzoah <147485458+ReginaldErzoah@users.noreply.github.com>
2026-03-11 11:20:49 +0000

    Update credit_card_fraud_streamlit_app.py

--- credit_card_fraud_streamlit_app.py
+++ credit_card_fraud_streamlit_app.py
@@ -6,10 +6,18 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-# Fix for SHAP + NumPy compatibility
+# --- Compatibility patch for SHAP with new NumPy versions ---
+if not hasattr(np, "bool"):
+    np.bool = bool
 if not hasattr(np, "int"):
     np.int = int
-
+if not hasattr(np, "float"):
+    np.float = float
+if not hasattr(np, "object"):
+    np.object = object
+if not hasattr(np, "str"):
+    np.str = str
+    
 import joblib
 import matplotlib.pyplot as plt
 from pathlib import Path
@@ -282,5 +290,6 @@ st.download_button(
     file_name="fraud_predictions.csv",
     mime="text/csv"
 )
+
 
 
